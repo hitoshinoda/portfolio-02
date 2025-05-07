f4ea87fbcc07ee516852cb0247c1e4b2665f1929
hitoshinoda <jinchannel7@gmail.com>
2025-05-07 16:10:48 +0900

    Fix github pages

--- src/components/SectionData.ts
+++ src/components/SectionData.ts
@@ -1,4 +1,4 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "portfolio-02";
 
 export interface SectionContentConfig {
   id: string;
